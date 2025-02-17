191f7cffadbc0339a07a34fed8828761acddc5c3
CodeWithEhtisham <shamikhan0347@gmail.com>
2025-02-17 17:26:42 +0500

    rename apps

--- analytics/apps.py
+++ analytics/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class ListenerConfig(AppConfig):
+class AnalyticsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'listener'
+    name = 'analytics'
