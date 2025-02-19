d00c346fb4dc61a2fbffbaa948130078b7d94890
CodeWithEhtisham <shamikhan0347@gmail.com>
2025-02-19 19:16:47 +0500

    endpoints added

--- analytics/urls.py
+++ analytics/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from . import views
+from .views import GetAnalyticsData, AddAnalyticsData
 
 urlpatterns = [
-    # path('', views.home, name='home'),  
+    path('get/', GetAnalyticsData.as_view(), name='get-analytics-data'),
+    path('add/', AddAnalyticsData.as_view(), name='add-analytics-data'),
 ]
